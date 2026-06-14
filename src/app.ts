99f60314c9fb3ee9b0e945cdb0ab3b4fb8129826
Agus Ocampo <agusocampo145@hotmail.com>
2026-06-14 12:04:40 -0300

    Mininum fix

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,13 @@ import 'dotenv/config';
 import express, { Request, Response, NextFunction } from 'express';
 import urlRoutes from './routes/urlRoutes';
 import { errorHandler } from './middlewares/errorHandler';
+import path from 'path';
 
 const app = express();
 
 app.use(express.json());
 app.use('/', urlRoutes);
-
+app.use(express.static(path.join(__dirname, 'public')));
 app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
   const statusMap: Record<string, number> = {
     'URL not found': 404,
